0e493dd6962adee8da3049ff1391f6c39f7b8d76
Kurt Mueller <kurtmueller42@gmail.com>
2022-04-17 19:29:19 -0400

    Making progress

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/signal"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -29,304 +33,17 @@ func init() {
 }
 
 var (
-	integerOptionMinValue = 1.0
-
 	objects = []string{
 		"mimi", "momo", "mimo", "tree", "birb", "shiro",
 	}
 
+	bucket = "www.momobot.net"
+
 	commands = []*discordgo.ApplicationCommand{}
-	// {
-	// 	Name: "basic-command",
-	// 	// All commands and options must have a description
-	// 	// Commands/options without description will fail the registration
-	// 	// of the command.
-	// 	Description: "Basic command",
-	// },
-	// {
-	// 	Name:        "basic-command-with-files",
-	// 	Description: "Basic command with files",
-	// },
-	// {
-	// 	Name:        "subcommands",
-	// 	Description: "Subcommands and command groups example",
-	// 	Options: []*discordgo.ApplicationCommandOption{
-	// 		// When a command has subcommands/subcommand groups
-	// 		// It must not have top-level options, they aren't accesible in the UI
-	// 		// in this case (at least not yet), so if a command has
-	// 		// subcommands/subcommand any groups registering top-level options
-	// 		// will cause the registration of the command to fail
-	// 		{
-	// 			Name:        "subcommand-group",
-	// 			Description: "Subcommands group",
-	// 			Options: []*discordgo.ApplicationCommandOption{
-	// 				// Also, subcommand groups aren't capable of
-	// 				// containing options, by the name of them, you can see
-	// 				// they can only contain subcommands
-	// 				{
-	// 					Name:        "nested-subcommand",
-	// 					Description: "Nested subcommand",
-	// 					Type:        discordgo.ApplicationCommandOptionSubCommand,
-	// 				},
-	// 			},
-	// 			Type: discordgo.ApplicationCommandOptionSubCommandGroup,
-	// 		},
-	// 		// Also, you can create both subcommand groups and subcommands
-	// 		// in the command at the same time. But, there's some limits to
-	// 		// nesting, count of subcommands (top level and nested) and options.
-	// 		// Read the intro of slash-commands docs on Discord dev portal
-	// 		// to get more information
-	// 		{
-	// 			Name:        "subcommand",
-	// 			Description: "Top-level subcommand",
-	// 			Type:        discordgo.ApplicationCommandOptionSubCommand,
-	// 		},
-	// 	},
-	// },
-	// {
-	// 	Name:        "responses",
-	// 	Description: "Interaction responses testing initiative",
-	// 	Options: []*discordgo.ApplicationCommandOption{
-	// 		{
-	// 			Name:        "resp-type",
-	// 			Description: "Response type",
-	// 			Type:        discordgo.ApplicationCommandOptionInteger,
-	// 			Choices: []*discordgo.ApplicationCommandOptionChoice{
-	// 				{
-	// 					Name:  "Channel message with source",
-	// 					Value: 4,
-	// 				},
-	// 				{
-	// 					Name:  "Deferred response With Source",
-	// 					Value: 5,
-	// 				},
-	// 			},
-	// 			Required: true,
-	// 		},
-	// 	},
-	// },
-	// {
-	// 	Name:        "followups",
-	// 	Description: "Followup messages",
-	// },
-	// }
 
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){}
-	// "basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-	// 		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	// 		Data: &discordgo.InteractionResponseData{
-	// 			Content: "Hey there! Congratulations, you just executed your first slash command",
-	// 		},
-	// 	})
-	// },
-	// "basic-command-with-files": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-	// 		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	// 		Data: &discordgo.InteractionResponseData{
-	// 			Content: "Hey there! Congratulations, you just executed your first slash command with a file in the response",
-	// 			Files: []*discordgo.File{
-	// 				{
-	// 					ContentType: "text/plain",
-	// 					Name:        "test.txt",
-	// 					Reader:      strings.NewReader("Hello Discord!!"),
-	// 				},
-	// 			},
-	// 		},
-	// 	})
-	// },
-	// "options": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// 	// Access options in the order provided by the user.
-	// 	options := i.ApplicationCommandData().Options
 
-	// 	// Or convert the slice into a map
-	// 	optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
-	// 	for _, opt := range options {
-	// 		optionMap[opt.Name] = opt
-	// 	}
-
-	// 	// This example stores the provided arguments in an []interface{}
-	// 	// which will be used to format the bot's response
-	// 	margs := make([]interface{}, 0, len(options))
-	// 	msgformat := "You learned how to use command options! " +
-	// 		"Take a look at the value(s) you entered:\n"
-
-	// 	// Get the value from the option map.
-	// 	// When the option exists, ok = true
-	// 	if option, ok := optionMap["attachment-option"]; ok {
-	// 		key := option.Value.(string)
-
-	// 		attachment_url := i.Data.(discordgo.ApplicationCommandInteractionData).Resolved.Attachments[key].URL
-	// 		fmt.Println(attachment_url)
-	// 		msgformat += "> attachment-option: %s\n"
-	// 	}
-
-	// 	if opt, ok := optionMap["integer-option"]; ok {
-	// 		margs = append(margs, opt.IntValue())
-	// 		msgformat += "> integer-option: %d\n"
-	// 	}
-
-	// 	if opt, ok := optionMap["number-option"]; ok {
-	// 		margs = append(margs, opt.FloatValue())
-	// 		msgformat += "> number-option: %f\n"
-	// 	}
-
-	// 	if opt, ok := optionMap["bool-option"]; ok {
-	// 		margs = append(margs, opt.BoolValue())
-	// 		msgformat += "> bool-option: %v\n"
-	// 	}
-
-	// 	if opt, ok := optionMap["channel-option"]; ok {
-	// 		margs = append(margs, opt.ChannelValue(nil).ID)
-	// 		msgformat += "> channel-option: <#%s>\n"
-	// 	}
-
-	// 	if opt, ok := optionMap["user-option"]; ok {
-	// 		margs = append(margs, opt.UserValue(nil).ID)
-	// 		msgformat += "> user-option: <@%s>\n"
-	// 	}
-
-	// 	if opt, ok := optionMap["role-option"]; ok {
-	// 		margs = append(margs, opt.RoleValue(nil, "").ID)
-	// 		msgformat += "> role-option: <@&%s>\n"
-	// 	}
-
-	// 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-	// 		// Ignore type for now, they will be discussed in "responses"
-	// 		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	// 		Data: &discordgo.InteractionResponseData{
-	// 			Content: fmt.Sprintf(
-	// 				msgformat,
-	// 				margs...,
-	// 			),
-	// 		},
-	// 	})
-	// },
-	// "subcommands": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// 	options := i.ApplicationCommandData().Options
-	// 	content := ""
-
-	// 	// As you can see, names of subcommands (nested, top-level)
-	// 	// and subcommand groups are provided through the arguments.
-	// 	switch options[0].Name {
-	// 	case "subcommand":
-	// 		content = "The top-level subcommand is executed. Now try to execute the nested one."
-	// 	case "subcommand-group":
-	// 		options = options[0].Options
-	// 		switch options[0].Name {
-	// 		case "nested-subcommand":
-	// 			content = "Nice, now you know how to execute nested commands too"
-	// 		default:
-	// 			content = "Oops, something went wrong.\n" +
-	// 				"Hol' up, you aren't supposed to see this message."
-	// 		}
-	// 	}
-
-	// 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-	// 		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	// 		Data: &discordgo.InteractionResponseData{
-	// 			Content: content,
-	// 		},
-	// 	})
-	// },
-	// "responses": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// 	// Responses to a command are very important.
-	// 	// First of all, because you need to react to the interaction
-	// 	// by sending the response in 3 seconds after receiving, otherwise
-	// 	// interaction will be considered invalid and you can no longer
-	// 	// use the interaction token and ID for responding to the user's request
-
-	// 	content := ""
-	// 	// As you can see, the response type names used here are pretty self-explanatory,
-	// 	// but for those who want more information see the official documentation
-	// 	switch i.ApplicationCommandData().Options[0].IntValue() {
-	// 	case int64(discordgo.InteractionResponseChannelMessageWithSource):
-	// 		content =
-	// 			"You just responded to an interaction, sent a message and showed the original one. " +
-	// 				"Congratulations!"
-	// 		content +=
-	// 			"\nAlso... you can edit your response, wait 5 seconds and this message will be changed"
-	// 	default:
-	// 		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-	// 			Type: discordgo.InteractionResponseType(i.ApplicationCommandData().Options[0].IntValue()),
-	// 		})
-	// 		if err != nil {
-	// 			s.FollowupMessageCreate(s.State.User.ID, i.Interaction, true, &discordgo.WebhookParams{
-	// 				Content: "Something went wrong",
-	// 			})
-	// 		}
-	// 		return
-	// 	}
-
-	// 	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-	// 		Type: discordgo.InteractionResponseType(i.ApplicationCommandData().Options[0].IntValue()),
-	// 		Data: &discordgo.InteractionResponseData{
-	// 			Content: content,
-	// 		},
-	// 	})
-	// 	if err != nil {
-	// 		s.FollowupMessageCreate(s.State.User.ID, i.Interaction, true, &discordgo.WebhookParams{
-	// 			Content: "Something went wrong",
-	// 		})
-	// 		return
-	// 	}
-	// 	time.AfterFunc(time.Second*5, func() {
-	// 		_, err = s.InteractionResponseEdit(s.State.User.ID, i.Interaction, &discordgo.WebhookEdit{
-	// 			Content: content + "\n\nWell, now you know how to create and edit responses. " +
-	// 				"But you still don't know how to delete them... so... wait 10 seconds and this " +
-	// 				"message will be deleted.",
-	// 		})
-	// 		if err != nil {
-	// 			s.FollowupMessageCreate(s.State.User.ID, i.Interaction, true, &discordgo.WebhookParams{
-	// 				Content: "Something went wrong",
-	// 			})
-	// 			return
-	// 		}
-	// 		time.Sleep(time.Second * 10)
-	// 		s.InteractionResponseDelete(s.State.User.ID, i.Interaction)
-	// 	})
-	// },
-	// "followups": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// 	// Followup messages are basically regular messages (you can create as many of them as you wish)
-	// 	// but work as they are created by webhooks and their functionality
-	// 	// is for handling additional messages after sending a response.
-
-	// 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-	// 		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	// 		Data: &discordgo.InteractionResponseData{
-	// 			// Note: this isn't documented, but you can use that if you want to.
-	// 			// This flag just allows you to create messages visible only for the caller of the command
-	// 			// (user who triggered the command)
-	// 			Flags:   1 << 6,
-	// 			Content: "Surprise!",
-	// 		},
-	// 	})
-	// 	msg, err := s.FollowupMessageCreate(s.State.User.ID, i.Interaction, true, &discordgo.WebhookParams{
-	// 		Content: "Followup message has been created, after 5 seconds it will be edited",
-	// 	})
-	// 	if err != nil {
-	// 		s.FollowupMessageCreate(s.State.User.ID, i.Interaction, true, &discordgo.WebhookParams{
-	// 			Content: "Something went wrong",
-	// 		})
-	// 		return
-	// 	}
-	// 	time.Sleep(time.Second * 5)
-
-	// 	s.FollowupMessageEdit(s.State.User.ID, i.Interaction, msg.ID, &discordgo.WebhookEdit{
-	// 		Content: "Now the original message is gone and after 10 seconds this message will ~~self-destruct~~ be deleted.",
-	// 	})
-
-	// 	time.Sleep(time.Second * 10)
-
-	// 	s.FollowupMessageDelete(s.State.User.ID, i.Interaction, msg.ID)
-
-	// 	s.FollowupMessageCreate(s.State.User.ID, i.Interaction, true, &discordgo.WebhookParams{
-	// 		Content: "For those, who didn't skip anything and followed tutorial along fairly, " +
-	// 			"take a unicorn :unicorn: as reward!\n" +
-	// 			"Also, as bonus... look at the original interaction response :D",
-	// 	})
-	// },
-	// }
+	momos []string
 )
 
 func init() {
@@ -353,19 +70,32 @@ func main() {
 
 	log.Println("Adding commands...")
 
-	// registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
-	// for i, v := range commands {
-	// 	cmd, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
-	// 	if err != nil {
-	// 		log.Panicf("Cannot create '%v' command: %v", v.Name, err)
-	// 	}
-	// 	registeredCommands[i] = cmd
-	// }
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+	out, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: &bucket,
+		Prefix: &objects[1],
+	})
+
+	if err != nil {
+		log.Printf("Unable to fetch momos with error %v\n", err)
+	} else {
+		momos = make([]string, len(out.Contents))
+
+		for i, obj := range out.Contents {
+			momos[i] = *obj.Key
+		}
+	}
 
 	_, err = s.ApplicationCommandBulkOverwrite(s.State.User.ID, *GuildID, commands)
 
 	if err != nil {
-		log.Println("FAILED to bulk overwrite commands")
+		log.Printf("FAILED to bulk overwrite commands because %v\n", err)
 	}
 
 	defer s.Close()
@@ -396,6 +126,16 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Content == "!honk" {
 		s.ChannelMessageSend(m.ChannelID, "HOOOONK")
 	}
+
+	if m.Content == "!momo" {
+		if len(momos) == 0 {
+			return
+		}
+
+		var n = rand.Intn(len(momos))
+
+		s.ChannelMessageSend(m.ChannelID, momos[n])
+	}
 }
 
 func populateCommandInfo(commandHandlers map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate),
@@ -411,7 +151,7 @@ func populateCommandInfo(commandHandlers map[string]func(s *discordgo.Session, i
 			Options: []*discordgo.ApplicationCommandOption{
 				{
 					Type:        discordgo.ApplicationCommandOptionAttachment,
-					Name:        obj + " to upload",
+					Name:        obj + "-to-upload",
 					Description: "upload a new " + obj,
 					Required:    true,
 				},
